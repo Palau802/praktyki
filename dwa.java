ad782d37c618ede08e1bc17e4bc2996efc1746f1
Palau802 <79452535+Palau802@users.noreply.github.com>
2021-02-23 17:32:12 +0100

    Add files via upload

---
+++ dwa.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class dwa {
+    public static void main(String[] args) {
+
+        double a;
+
+        a = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+
+
+        System.out.println("twój wynik wynosi" + a);
+    }
+}
