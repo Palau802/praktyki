0dcb1586be76a4a11f26f06ebbf0b9f8cd26e45e
Palau802 <79452535+Palau802@users.noreply.github.com>
2021-02-24 14:42:10 +0100

    Add files via upload

---
+++ jeden.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args) {
+
+                System.out.println("Ania");
+
+                System.out.println("Bartek");
+
+                System.out.println("Kasia");
+    }
+}
